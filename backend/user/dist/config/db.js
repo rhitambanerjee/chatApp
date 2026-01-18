02e9ad622da1d5b3879e00269e5d468651c18994
rhitambanerjee <rb2403@it.jgec.ac.in>
2026-01-18 12:12:00 +0530

    initial commit

---
+++ backend/user/dist/config/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+const connectDB = async () => {
+    const url = process.env.MONGO_URI;
+    if (!url) {
+        throw new Error("Can't find mongo url");
+    }
+    try {
+        await mongoose.connect(url, {
+            dbName: "chatAppMicroService"
+        });
+        console.log("Connected to chatapp successful");
+    }
+    catch (error) {
+        console.log("Failed to connect to mongodb", error);
+        process.exit(1);
+    }
+};
+export default connectDB;
+//# sourceMappingURL=db.js.map
