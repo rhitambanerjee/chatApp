9d44747ed45193eedee0c184448339a6364f287a
rhitambanerjee <rb2403@it.jgec.ac.in>
2026-02-01 09:59:38 +0530

    added trycatch format

---
+++ backend/user/src/config/TryCatch.ts
@@ -0,0 +1,19 @@
+import type { NextFunction, Request, RequestHandler, Response } from "express";
+
+export const TryCatch = (handler: RequestHandler): RequestHandler => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        await handler(req, res, next);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        res.status(500).json({
+          message: error.message,
+        });
+      } else {
+        res.status(500).json({
+          message: "Something went wrong",
+        });
+      }
+    }
+  };
+};
