bd67cc85008eda72d9cc6a28ae78b96a0e5a7bcf
rhitambanerjee <rb2403@it.jgec.ac.in>
2026-03-27 00:24:57 +0530

    added changes for email handling

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,24 +1,73 @@
-import { Mail } from 'lucide-react'
-import React from 'react'
+"use client";
+import axios from "axios";
+import { ArrowRight, Mail } from "lucide-react";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [email, setEmail] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+  const router=useRouter();
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>):Promise<void> => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+        const {data} = await axios.post(`http://localhost:5000/api/v1/login`, { email });
+        alert(data.message);
+        router.push(`/verify?email=${email}`);
+    } catch (error:any) {
+        alert(error.response.data.message);
+    }finally{
+        setLoading(false);
+    }
+  }
   return (
-    <div className='min-h-screen bg-gray-900 flex items-center justify-center p-4'>
-        <div className='max-w-md w-full '>
-            <div className='bg-gray-800 border border-gray-700 rounded-lg p-8'>
-                <div className='text-center mb-8'>
-                    <div className='mx-auto w-20 h-20 bg-blue-600 rounded-lg flex items-center justify-center mb-6'>
-                        <Mail size={40} className='text-white'/>
-                    </div>
-                    <h1 className="text-4xl font-bold text-white mb-3 ">
-                        Welcome to ChatApp
-                    </h1>
-                    <p className="text-gray-300 text-lg">Enter your email to continue your journey</p>
-                </div>
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center p-4">
+      <div className="max-w-md w-full ">
+        <div className="bg-gray-800 border border-gray-700 rounded-lg p-8">
+          <div className="text-center mb-8">
+            <div className="mx-auto w-20 h-20 bg-blue-600 rounded-lg flex items-center justify-center mb-6">
+              <Mail size={40} className="text-white" />
             </div>
+            <h1 className="text-4xl font-bold text-white mb-3 ">
+              Welcome to ChatApp
+            </h1>
+            <p className="text-gray-300 text-lg">
+              Enter your email to continue your journey
+            </p>
+          </div>
+          <form onSubmit={handleSubmit} className="space-y-6">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-300 mb-2"
+              >
+                Email Address
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="w-full px-4 py-4 bg-gray-700 border-gray-600 rounded-lg text-white  placeholder-gray-400"
+                placeholder="Enter your email address"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-blue-600 px-6 py-4 rounded-lg text-white font-semibold hover:bg-blue-700 disabled:opacity-40 disabled:cursor-not-allowed cursor-pointer"
+            >
+              <div className="flex items-center justify-center gap-2">
+                <span>Send verification code</span>
+                <ArrowRight className="h-5 w-5" />
+              </div>
+            </button>
+          </form>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
